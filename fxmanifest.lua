db4b120a3e8a4b1b134e04b0b68a2292a32162e0
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2023-08-17 14:47:08 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Zancudo Alarm/Military personnel agression resource'
+
+version '1.0.0'
+
+client_scripts {
+	'client.lua'
+}
+
+shared_scripts {
+	'@ox_lib/init.lua',
+}
+
+lua54 'yes'
